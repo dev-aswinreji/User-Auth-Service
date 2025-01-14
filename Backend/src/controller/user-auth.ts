d0ce324ca74895c5746ebe49c88b6a23034899b6
Aswin Reji <aswinreji.004@gmail.com>
2025-01-14 17:43:27 +0000

    feat: unwanted lines removed

--- Backend/src/controller/user-auth.ts
+++ Backend/src/controller/user-auth.ts
@@ -2,9 +2,6 @@ import { Request, Response } from "express";
 import client from "../model/postgres";
 export async function userAuth(req: Request, res: Response): Promise<any> {
   try {
-
-    const data = await client.query("SELECT * FROM Users")
-    console.log(data, 'data is here');
     return res.status(200).json({ message: "Login auth success" });
 
   } catch (error) {
