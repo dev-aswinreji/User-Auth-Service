e6b79d9909df1c0ad858cd35d6d5fe82559fcb8d
Aswin Reji <aswinreji.004@gmail.com>
2025-01-16 13:35:59 +0000

    feat: backend call is not working

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   const getLoginData = async () => {
     console.log('Hello world');
     const result = await axios.get("http://localhost:4000/login")
-    console.log(result,'result is here ');
+    console.log(result, 'result is here ');
 
     setLoginData(result.data.message)
   }
