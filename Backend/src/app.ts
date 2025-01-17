ca552c953f9122c271928890e19438e651b14616
Aswin Reji <aswinreji.004@gmail.com>
2025-01-17 16:35:55 +0000

    feat: backend call is not working

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -4,12 +4,16 @@ import route from "./route/user-route";
 
 const app = express();
 
-app.use(cors<Request>())
+// app.use(cors<Request>())
 const PORT = process.env.PORT || 4000;
 console.log(PORT, "Port is showing");
 
-app.use("/",route)
+// app.use("/",route)
 
-app.listen(PORT, () => {
+app.get("/", async (req: Request, res: Response) => {
+  res.send("hello world")
+})
+
+app.listen(4000, () => {
   console.log(`Server is listening http://localhost:${PORT}/login`);
 });
