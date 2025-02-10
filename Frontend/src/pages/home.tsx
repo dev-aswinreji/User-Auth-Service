62f355141f17e9a91afd0b92551b8411dde8b9ee
Aswin Reji <aswinreji.004@gmail.com>
2025-02-10 17:48:48 +0000

    fix: modified backend port on home and service

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -1,22 +1,30 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { backendHomeEntrypoint, backendLocator, backendPort } from "../utils/url"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
     const [home, setHome] = useState("")
+    const navigator = useNavigate()
     async function GetHome() {
-        const { data } = await axios.get(`${backendLocator}${backendPort}${backendHomeEntrypoint}`)
+        const { data, status } = await axios.get(`${backendLocator}${backendPort}${backendHomeEntrypoint}`)
+        if (status !== 200) {
+            navigator("/signup")
+        }
         setHome(data.message)
     }
-    console.log(home, 'home is shoeing');
+
     useEffect(() => {
         GetHome()
     }, [home])
+
     return (
 
         <>
             <h1>Home is here</h1>
-            {home}
+            <h2>
+                {home}
+            </h2>
         </>
     )
 }
