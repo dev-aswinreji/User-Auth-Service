c4ed00d0f85fdb91712ad64a94af3ae7876054cf
Aswin Reji <aswinreji.004@gmail.com>
2025-02-15 17:42:26 +0000

    fix: refractored the code of auth

--- Backend/src/controller/user-auth.ts
+++ Backend/src/controller/user-auth.ts
@@ -2,32 +2,17 @@ import { Request, Response } from "express";
 import client from "../model/postgres";
 export async function userAuth(req: Request, res: Response): Promise<any> {
   try {
-    console.log('inside post method');
-
-
-
     const { name, email, password } = req.body
-
-    console.log(email, 'Email is here');
 
     const isEmailExist = await client.query(`SELECT email FROM Users WHERE email='${email}'`)
 
-    console.log(isEmailExist, 'is email exist is what');
-
-    console.log(isEmailExist.rows[0]?.email, 'is email exist is what');
-
-
     const result = isEmailExist.rows[0]?.email ? { message: 'email already exists', statusCode: 409 } : { message: "SignUp success", statusCode: 201 }
 
-    console.log(result, 'Result is showing');
-
     if (result.statusCode === 201) {
-      const isInserted = await client.query(`INSERT INTO Users 
+      await client.query(`INSERT INTO Users 
         (name,email,password)
         VALUES('${name}','${email}','${password}')
         `)
-      console.log(isInserted, 'is inserted');
-
     }
 
     res.status(result.statusCode).json({ message: result.message })
